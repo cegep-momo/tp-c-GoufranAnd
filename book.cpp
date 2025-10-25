611b5c221436a7b6ba0eab09038452d0ab1f9868
goufran <goufran.andoulsi@hotmail.com>
2025-10-25 11:03:03 -0400

    Getters et setters

--- book.cpp
+++ book.cpp
@@ -11,10 +11,44 @@ Book::Book():
 {}
 
 //Constructeur avec param
-Book::Book(const string& titre, const string& auteur, const string& isbn){
+Book::Book(const string& titre, const string& auteur, const string& _isbn){
     this->title = titre;
     this->author = auteur;
-    this->isbn = isbn;
+    this->isbn = _isbn;
     this->isAvailable = true;
     this->borrowerName = "";
+}
+
+//getters
+Book::string getTitle() const {
+    return title;
+}
+Book::string getAuthor() const{
+    return author;
+}
+Book::string getISBN() const {
+    return isbn;
+}
+Book::bool getAvailability() const {
+    return isAvaible;
+}
+Book::string getBorrowerName() const{
+    return borrowerName;
+}
+
+//setters
+Book::void setTitle(const string& titre) {
+    title = titre;
+}
+Book::void setAuthor(const string& auteur){
+    author = auteur;
+}
+Book::void setISBN(const string& _isbn){
+    isbn = _isbn;
+}
+Book::void setAvailability(bool available){
+    isAvailable = available;
+}
+Book::void setBorrowerName(const string& name){
+    borrowerName = name;
 }
